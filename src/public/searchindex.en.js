8973d3b8e14554db8815a6150f3e8cf99901c020
IT Fight Club <itfightclub@pm.me>
2024-11-12 22:05:34 -0500

    feat(hugo): Create Hugo with relearn theme
    Create Hugo site with relearn theme. Also configured basic dev container.

---
+++ src/public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "tech-lingo \u003e Posts",
+    "content": "Introduction This is bold text, and this is emphasized text.\nVisit the Hugo website!\nNew Content.",
+    "description": "Introduction This is bold text, and this is emphasized text.\nVisit the Hugo website!\nNew Content.",
+    "tags": [],
+    "title": "My First Post",
+    "uri": "/itfightclub/tech-lingo/tree/docs/posts/my-first-post/index.html"
+  },
+  {
+    "breadcrumb": "tech-lingo",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/itfightclub/tech-lingo/tree/docs/posts/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "tech-lingo",
+    "uri": "/itfightclub/tech-lingo/tree/docs/index.html"
+  },
+  {
+    "breadcrumb": "tech-lingo",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/itfightclub/tech-lingo/tree/docs/categories/index.html"
+  },
+  {
+    "breadcrumb": "tech-lingo",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/itfightclub/tech-lingo/tree/docs/tags/index.html"
+  }
+]
